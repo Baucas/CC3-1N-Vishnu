89dfde12b8534816ee737dca830f4ff18c1fb2b4
Baucas <153827577+Baucas@users.noreply.github.com>
2024-03-04 00:05:08 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CC3_1N_Vishnu;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
